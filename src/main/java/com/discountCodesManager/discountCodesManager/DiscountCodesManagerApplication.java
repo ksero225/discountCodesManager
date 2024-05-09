ae07253b9022b849c644657950aecbb2dbd75600
Paweł Michalski <pmpawel126@gmail.com>
2024-05-09 20:48:58 +0200

    First commit. Project created

---
+++ src/main/java/com/discountCodesManager/discountCodesManager/DiscountCodesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.discountCodesManager.discountCodesManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCodesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountCodesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discountCodesManager/discountCodesManager/DiscountCodesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discountCodesManager.discountCodesManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountCodesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
