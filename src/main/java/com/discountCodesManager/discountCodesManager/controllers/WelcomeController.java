1b5c851dc269a44b757a7c7f4adddc3a4cc7a32f
Paweł Michalski <pmpawel126@gmail.com>
2024-05-14 22:01:59 +0200

    Added WelcomeController and README file. Also exported postman collections

---
+++ src/main/java/com/discountCodesManager/discountCodesManager/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.discountCodesManager.discountCodesManager.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(path = "/")
+    public String welcome(){
+        return "Welcome, this is discount codes manager for Sii LAT. Look for Postman collections in 'postmanCollections' catalog for samples query";
+    }
+}
