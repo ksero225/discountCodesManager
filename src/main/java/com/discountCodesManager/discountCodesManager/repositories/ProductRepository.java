d34c337b88112004cfb313a1551fa2352609650e
Paweł Michalski <pmpawel126@gmail.com>
2024-05-15 22:04:12 +0200

    .

--- src/main/java/com/discountCodesManager/discountCodesManager/repositories/ProductRepository.java
+++ src/main/java/com/discountCodesManager/discountCodesManager/repositories/ProductRepository.java
@@ -3,6 +3,8 @@ package com.discountCodesManager.discountCodesManager.repositories;
 import com.discountCodesManager.discountCodesManager.domain.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>, PagingAndSortingRepository<ProductEntity, Long> {
 }
--- src/main/java/com/discountCodesManager/discountCodesManager/repositories/PromoCodeRepository.java
+++ src/main/java/com/discountCodesManager/discountCodesManager/repositories/PromoCodeRepository.java
@@ -4,9 +4,11 @@ import com.discountCodesManager.discountCodesManager.domain.entities.PromoCodeEn
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PromoCodeRepository extends JpaRepository<PromoCodeEntity, Long>, PagingAndSortingRepository<PromoCodeEntity, Long> {
     boolean existsByPromoCode(String promoCode);
     Optional<PromoCodeEntity> findPromoCodeEntityByPromoCode(String promoCode);
