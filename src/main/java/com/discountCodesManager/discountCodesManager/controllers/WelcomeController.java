9e96e036cd1aa8609a7a045a36545804b04b725d
Paweł Michalski <pmpawel126@gmail.com>
2024-06-06 18:30:59 +0200

    .

--- src/main/java/com/discountCodesManager/discountCodesManager/controllers/WelcomeController.java
+++ src/main/java/com/discountCodesManager/discountCodesManager/controllers/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping(path = "/")
     public String welcome(){
-        return "Welcome, this is discount codes manager for Sii LAT. Look for Postman collections in 'postmanCollections' catalog for samples query";
+        return "Welcome, this is discount codes manager. Look for Postman collections in 'postmanCollections' catalog for samples query";
     }
 }
